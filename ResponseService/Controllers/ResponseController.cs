fc67f0c8a176853ebb7253338f2995fa225e37a4
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-03-20 14:38:16 +0600

    Created Response Controller

---
+++ ResponseService/Controllers/ResponseController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ResponseService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ResponseController : ControllerBase
+    {
+        [Route("{id:int}")]
+        [HttpGet]
+        public ActionResult GetResponse(int id)
+        {
+            return Ok(id);  
+        }
+
+    }
+}
