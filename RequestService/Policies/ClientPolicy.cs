df9ea96b10271f638ff0d3cc5ae545ebf3f3f153
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-03-27 14:14:07 +0600

    Update ClientPolicy.cs

--- RequestService/Policies/ClientPolicy.cs
+++ RequestService/Policies/ClientPolicy.cs
@@ -6,11 +6,16 @@ namespace RequestService.Policies
     public class ClientPolicy
     {
         public AsyncRetryPolicy<HttpResponseMessage> ImmidiateHttpPolicy { get; }
+        public AsyncRetryPolicy<HttpResponseMessage> LinearHttpPolicy { get; }
 
         public ClientPolicy()
         {
-            ImmidiateHttpPolicy =Policy.HandleResult<HttpResponseMessage>(
-                res=> !res.IsSuccessStatusCode).RetryAsync(5);
+            ImmidiateHttpPolicy = Policy.HandleResult<HttpResponseMessage>(
+                res => !res.IsSuccessStatusCode).RetryAsync(5);
+
+            LinearHttpPolicy = Policy.HandleResult<HttpResponseMessage>(
+                res => !res.IsSuccessStatusCode).WaitAndRetryAsync(5, retryAttempt => TimeSpan.FromSeconds(3));
+
         }
     }
 }
