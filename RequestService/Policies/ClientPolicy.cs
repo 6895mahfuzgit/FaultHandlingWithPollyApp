a53a5a864817326be37d6b9ec44449b5975db2b0
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-03-27 14:08:56 +0600

    Create ClientPolicy.cs

---
+++ RequestService/Policies/ClientPolicy.cs
@@ -0,0 +1,16 @@
+﻿using Polly;
+using Polly.Retry;
+
+namespace RequestService.Policies
+{
+    public class ClientPolicy
+    {
+        public AsyncRetryPolicy<HttpResponseMessage> ImmidiateHttpPolicy { get; }
+
+        public ClientPolicy()
+        {
+            ImmidiateHttpPolicy =Policy.HandleResult<HttpResponseMessage>(
+                res=> !res.IsSuccessStatusCode).RetryAsync(5);
+        }
+    }
+}
